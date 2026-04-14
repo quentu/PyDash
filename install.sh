c161ef5b1cee4b2ca412de6008b31eddbcb8e429
Quentin Ustoy <qustoy@proton.me>
2026-04-14 16:24:15 -0700

    Updated dependencies for install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ source venv/bin/activate
 
 # Install Python deps
 pip install --upgrade pip
-pip install fastapi uvicorn psutil
+pip install fastapi uvicorn psutil nvidia-ml-py
 
 # Download agent.py
 curl -sSL https://raw.githubusercontent.com/quentu/PyDash/main/agent/agent.py -o agent.py
